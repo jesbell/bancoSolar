412d02d83655ba86b8e4d3dc0b6c1be3d45cde79
jesbell <joselyngonzalezma@gmail.com>
2024-04-29 22:43:07 -0400

    Agregando archivo utils para crear allí la función que nos generara la fecha y hora cada vez que se haga una transferencia

---
+++ utils.js
@@ -0,0 +1,20 @@
+function obtenerFechaHoraActual() {
+    // Obtener la fecha y hora actuales
+    const now = new Date();
+
+    // Obtener la fecha
+    const dia = String(now.getDate()).padStart(2, '0');
+    const mes = String(now.getMonth() + 1).padStart(2, '0'); 
+    const año = now.getFullYear(); 
+
+    // Obtener la hora
+    const horas = String(now.getHours()).padStart(2, '0');
+    const minutos = String(now.getMinutes()).padStart(2, '0');
+
+    // Crear la cadena de fecha y hora
+    const fechaHora = `${dia}/${mes}/${año} ${horas}:${minutos}`;
+
+    return fechaHora;
+}
+
+module.exports = { obtenerFechaHoraActual };
